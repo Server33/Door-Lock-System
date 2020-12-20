42e0c4d8cb88f5d6edcc5583e4566b7f72001f8f
Server33 <ibrahim.oezkan2@gmail.com>
2020-12-20 15:09:36 +0100

    Hello

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+port = process.env.PORT || 80;
+
+app.get("/",(req,res)=>{
+    res.send("Hello ");
+    console.log("Logged in");
+})
+
+app.get("/:id",(req,res)=>{
+    res.send("Hello " + req.params.id);
+    console.log("Logged in");
+})
+
+
+app.listen(port,(err)=>{
+
+});
